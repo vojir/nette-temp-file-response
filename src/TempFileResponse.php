7ba4e5e3c9844b5018a15938fb2c17b6596a4693
Stanislav Vojíř <stanislav.vojir@gmail.com>
2019-10-21 21:31:27 +0200

    update pro Nette 3

--- src/TempFileResponse.php
+++ src/TempFileResponse.php
@@ -9,9 +9,23 @@ use Nette;
  * @package Vojir\Responses\TempFileResponse
  * @author Stanislav Vojíř
  */
-class TempFileResponse extends Nette\Application\Responses\FileResponse implements Nette\Application\IResponse{
-  /** @var string $file */
+final class TempFileResponse implements Nette\Application\IResponse{
+  use Nette\SmartObject;
+
+  /** @var bool */
+  public $resuming = true;
+
+  /** @var string */
   private $file;
+
+  /** @var string */
+  private $contentType;
+
+  /** @var string */
+  private $name;
+
+  /** @var bool */
+  private $forceDownload;
 
   /**
    * TempFileResponse constructor.
@@ -21,13 +35,89 @@ class TempFileResponse extends Nette\Application\Responses\FileResponse implemen
    * @param bool $forceDownload = true
    * @throws Nette\Application\BadRequestException
    */
-  public function __construct($file, $name = null, $contentType = null, $forceDownload = true){
-    parent::__construct($file, $name, $contentType, $forceDownload);
-    $this->file=$file;
+  public function __construct(string $file, string $name = null, string $contentType = null, bool $forceDownload = true){
+    if (!is_file($file)) {
+      throw new Nette\Application\BadRequestException("File '$file' doesn't exist.");
+    }
+
+    $this->file = $file;
+    $this->name = $name ?? basename($file);
+    $this->contentType = $contentType ?: 'application/octet-stream';
+    $this->forceDownload = $forceDownload;
   }
 
-  public function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse){
-    parent::send($httpRequest, $httpResponse);
+
+  /**
+   * Returns the path to a downloaded file.
+   */
+  public function getFile(): string{
+    return $this->file;
+  }
+
+
+  /**
+   * Returns the file name.
+   */
+  public function getName(): string{
+    return $this->name;
+  }
+
+
+  /**
+   * Returns the MIME content type of a downloaded file.
+   */
+  public function getContentType(): string{
+    return $this->contentType;
+  }
+
+
+  /**
+   * Sends response to output.
+   */
+  public function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse): void{
+    $httpResponse->setContentType($this->contentType);
+    $httpResponse->setHeader('Content-Disposition',
+      ($this->forceDownload ? 'attachment' : 'inline')
+      . '; filename="' . $this->name . '"'
+      . '; filename*=utf-8\'\'' . rawurlencode($this->name));
+
+    $filesize = $length = filesize($this->file);
+    $handle = fopen($this->file, 'r');
+
+    if ($this->resuming) {
+      $httpResponse->setHeader('Accept-Ranges', 'bytes');
+      if (preg_match('#^bytes=(\d*)-(\d*)\z#', (string) $httpRequest->getHeader('Range'), $matches)) {
+        [, $start, $end] = $matches;
+        if ($start === '') {
+          $start = max(0, $filesize - $end);
+          $end = $filesize - 1;
+
+        } elseif ($end === '' || $end > $filesize - 1) {
+          $end = $filesize - 1;
+        }
+        if ($end < $start) {
+          $httpResponse->setCode(416); // requested range not satisfiable
+          return;
+        }
+
+        $httpResponse->setCode(206);
+        $httpResponse->setHeader('Content-Range', 'bytes ' . $start . '-' . $end . '/' . $filesize);
+        $length = $end - $start + 1;
+        fseek($handle, (int) $start);
+
+      } else {
+        $httpResponse->setHeader('Content-Range', 'bytes 0-' . ($filesize - 1) . '/' . $filesize);
+      }
+    }
+
+    $httpResponse->setHeader('Content-Length', (string) $length);
+    while (!feof($handle) && $length > 0) {
+      echo $s = fread($handle, min(4000000, $length));
+      $length -= strlen($s);
+    }
+    fclose($handle);
+
+    //delete temp file
     unlink($this->file);
   }
 
